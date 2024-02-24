44d82978239de1b2b3ceed645222e922cff9c600
Emir <emirsoyalan79@gmail.com>
2024-02-24 16:43:23 +0300

    fix concurrent request bottleneck issue

--- scripts/ip_search.py
+++ scripts/ip_search.py
@@ -13,13 +13,13 @@ from sqlalchemy.exc import SQLAlchemyError
 
 MAX_CONCURRENT_REQUESTS = 1000
 
+semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 db_adapter = DBAdapter("sqlite:///db/ip.db")
 ip_service = IPService(db_adapter)
 print("initial ips:", len(ip_service.get_ips()))
 
-async def scan_ip(ip, ports):
-    semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
-    async with semaphore, aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
+async def ip_scan_task(ip, ports, semaphore=semaphore):
+    async with semaphore, aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=CLIENT_TIMEOUT)) as session:
         for port in ports:
             try:
                 is_https = port == 443
@@ -61,18 +61,18 @@ async def scan_ip(ip, ports):
 async def ip_range_scan_task(ip_ranges = ((0, 16), (0, 16), (0, 16), (0, 16)), ports = (80, 443)):
     tasks = []
     
-    for a in range(ip_ranges[0][0], ip_ranges[0][1]):
+    for a in tqdm(range(ip_ranges[0][0], ip_ranges[0][1]), desc=f"Queueing IPs for chunk {ip_ranges}"):
         for b in range(ip_ranges[1][0], ip_ranges[1][1]):
             for c in range(ip_ranges[2][0], ip_ranges[2][1]):
                 for d in range(ip_ranges[3][0], ip_ranges[3][1]):
                     ip = f"{a}.{b}.{c}.{d}"
-                    tasks.append(scan_ip(ip, ports=ports))
+                    tasks.append(ip_scan_task(ip, ports=ports))
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     print("Starting IP search...")
 
-    chunk_size = 16
+    chunk_size = 8
     chunks = []
     from tqdm import tqdm
     for a in tqdm(range(0, 256, chunk_size), total=256//chunk_size, desc="Generating chunks"):
