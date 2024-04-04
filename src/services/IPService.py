20f3c6bb02fe3ddb52d0e71b51b7c1a7736d3c59
Emir <emirsoyalan79@gmail.com>
2024-04-04 04:34:08 +0300

    fix ip service commit issue

--- src/services/IPService.py
+++ src/services/IPService.py
@@ -11,56 +11,53 @@ class IPService(BaseService):
     
     def add_ip(self, ip_obj: IPTable) -> IPTable:
         """Add a new IP to the database."""
-        with self.db_adapter.get_session() as session:
-            session.add(ip_obj)
-            # session.commit()
-            return ip_obj
+        session = self.db_adapter.get_session()
+        session.add(ip_obj)
+        return ip_obj
 
     def get_ips(self) -> List[IPTable]:
         """Get all IPs from the database."""
-        with self.db_adapter.get_session() as session:
-            return session.query(IPTable).all()
+        return self.db_adapter.get_session().query(IPTable).all()
     
     def get_ip(self, ip: str) -> Optional[IPTable]:
         """Get a specific IP from the database."""
-        with self.db_adapter.get_session() as session:
-            return session.query(IPTable).filter_by(ip=ip).first()
+        self.db_adapter.get_session().query(IPTable).filter_by(ip=ip).first()
 
     def update_ip(self, new_ip_obj: IPTable) -> IPTable:
         """Update an existing IP in the database."""
-        with self.db_adapter.get_session() as session:
-            existing_ip_obj = session.query(IPTable).filter_by(ip=new_ip_obj.ip).first()
-            existing_ip_obj.domain = new_ip_obj.domain
-            existing_ip_obj.port = new_ip_obj.port
-            existing_ip_obj.status = new_ip_obj.status
-            existing_ip_obj.updated_at = datetime.now()
-            existing_ip_obj.keywords = new_ip_obj.keywords or existing_ip_obj.keywords
-            existing_ip_obj.title = new_ip_obj.title or existing_ip_obj.title
-            existing_ip_obj.description = new_ip_obj.description or existing_ip_obj.description
-            existing_ip_obj.body = new_ip_obj.body or existing_ip_obj.body
-            # session.commit()
-            return existing_ip_obj
+        session = self.db_adapter.get_session() 
+        existing_ip_obj = session.query(IPTable).filter_by(ip=new_ip_obj.ip).first()
+        existing_ip_obj.domain = new_ip_obj.domain
+        existing_ip_obj.port = new_ip_obj.port
+        existing_ip_obj.status = new_ip_obj.status
+        existing_ip_obj.updated_at = datetime.now()
+        existing_ip_obj.keywords = new_ip_obj.keywords or existing_ip_obj.keywords
+        existing_ip_obj.title = new_ip_obj.title or existing_ip_obj.title
+        existing_ip_obj.description = new_ip_obj.description or existing_ip_obj.description
+        existing_ip_obj.body = new_ip_obj.body or existing_ip_obj.body
+        return existing_ip_obj
 
     def delete_ip(self, ip: str) -> IPTable:
         """Delete a specific IP from the database."""
-        with self.db_adapter.get_session() as session:
-            ip_obj = session.query(IPTable).filter_by(ip=ip).first()
-            session.delete(ip_obj)
-            # session.commit()
-            return ip_obj
+        session = self.db_adapter.get_session()
+        ip_obj = session.query(IPTable).filter_by(ip=ip).first()
+        session.delete(ip_obj)
+        session.flush()
+        return ip_obj
 
     def delete_all_ips(self, commit) -> bool:
         """Delete all IPs from the database."""
-        with self.db_adapter.get_session() as session:
-            session.query(IPTable).delete()
-            if commit:
-                session.commit()
-            return True
+        session = self.db_adapter.get_session()
+        session.query(IPTable).delete()
+        if commit:
+            session.commit()
+        session.flush()
+        return True
     
     def get_valid_ips(self) -> List[IPTable]:
         """Get all valid IPs from the database."""
-        with self.db_adapter.get_session() as session:
-            return session.query(IPTable).filter_by(status=200).all()
+        session = self.db_adapter.get_session()
+        return session.query(IPTable).filter_by(status=200).all()
 
     def upsert_ip(self, new_ip_obj:IPTable) -> IPTable:
         """Add a new IP or update an existing one in the database."""
@@ -70,16 +67,8 @@ class IPService(BaseService):
             self.update_ip(new_ip_obj)
         else:
             self.add_ip(new_ip_obj)
-        # session.commit()
         return ip_obj
     
-    def commit(self):
-        """Commit the current transaction."""
-        if self.db_adapter.get_session().dirty \
-            or self.db_adapter.get_session().deleted \
-            or self.db_adapter.get_session().new:
-            self.db_adapter.get_session().commit()
-        print("Warning: No changes to commit.")
     
     # def queue_operation(self, func:str, *args, **kwargs):
     #     """Queue an operation to be executed later."""
@@ -91,7 +80,7 @@ class IPService(BaseService):
     #     """Execute all queued operations in a single transaction."""
     #     if not self._queue:
     #         return
-    #     with self.db_adapter.get_session() as session:
+    #     session = self.db_adapter.get_session()
     #         for func, args, kwargs in self._queue:
     #             func = str(func)
     #             self.__getattribute__(func)(*args, **kwargs)
