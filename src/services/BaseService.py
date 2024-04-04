8387852d935f41bfb78f442bb4dedf8e1a7ca03e
Emir <emirsoyalan79@gmail.com>
2024-04-04 13:01:19 +0300

    Add commit method to BaseService class

--- src/services/BaseService.py
+++ src/services/BaseService.py
@@ -6,3 +6,16 @@ class BaseService:
         """Initialize the BaseService with a DBAdapter."""
         self.db_adapter = db_adapter
         Base.metadata.create_all(self.db_adapter.engine)
+
+    def commit(self):
+        """Commit the current transaction."""
+        print("New:", len(self.db_adapter.persistent_session.new))
+        print("Updated:", len(self.db_adapter.persistent_session.dirty))
+        print("Deleted:", len(self.db_adapter.persistent_session.deleted))
+        if self.db_adapter.get_session().dirty \
+            or self.db_adapter.get_session().deleted \
+            or self.db_adapter.get_session().new:
+            self.db_adapter.get_session().commit()
+            print("Changes committed.")
+        else:
+            print("No changes to commit.")
