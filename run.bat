1925e491fc3fb6ddc46d6e754e033d1c0e5f4b83
Emir <emirsoyalan79@gmail.com>
2024-04-05 13:26:23 +0300

    Update run.bat to include titles for each command

--- run.bat
+++ run.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 rem Activate the Python environment
 call .\.env\scripts\activate
 
-start cmd /c "python ip_search.py"
-start cmd /c "python page_search.py"
-start cmd /c "python url_frontier_search.py"
+start cmd /c "title Crawler - IP Search & python ip_search.py"
+start cmd /c "title Crawler - Page Search & python page_search.py" 
+start cmd /c "title Crawler - URL Frontier & python url_frontier_search.py"
 
 endlocal
