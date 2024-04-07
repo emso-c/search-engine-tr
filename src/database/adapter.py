08910d11795ae5afde71728d4c460109a84a60c7
Emir <emirsoyalan79@gmail.com>
2024-04-07 16:58:32 +0300

    Refactor DBAdapter sync_with_remote method to use f-strings for remote_db_url

--- src/database/adapter.py
+++ src/database/adapter.py
@@ -36,54 +36,13 @@ class DBAdapter:
     def get_connection_type(self):
         """Return the connection type."""
         return self.engine.url.get_dialect().name
-    
-    # def sync_with_remote(self, remote_db_url: str):
-    #     """Sync the current database with a remote database."""
-    
-    #     remote_engine = create_engine(remote_db_url)
-    #     remote_metadata = MetaData()
-    #     remote_metadata.reflect(remote_engine)
-    #     remote_tables = remote_metadata.tables.keys()
-        
-    #     local_metadata = MetaData()
-    #     local_metadata.reflect(self.engine)
-    #     local_tables = local_metadata.tables.keys()
-        
-    #     for table in remote_tables:
-    #         if table not in local_tables:
-    #             remote_metadata.tables[table].create(self.engine)
-    #             print(f"Created table {table}")
-    #         else:
-    #             print(f"Table {table} already exists")
-        
-    #     # copying data
-    #     for table in remote_tables:
-    #         remote_table = remote_metadata.tables[table]
-    #         local_table = local_metadata.tables[table]
-            
-    #         remote_data = remote_engine.execute(remote_table.select()).fetchall()
-    #         local_data = self.engine.execute(local_table.select()).fetchall()
-            
-    #         for row in remote_data:
-    #             if row not in local_data:
-    #                 self.engine.execute(local_table.insert().values(row))
-    #                 print(f"Inserted row {row} into {table}")
-    #             else:
-    #                 print(f"Row {row} already exists in {table}")        
 
 
 def load_db_adapter(echo=False):
     try:
         db_adapter = DBAdapter(
-            url='mssql+pymssql://',
-            creator=lambda: pymssql.connect(
-                server=server,
-                user=user,
-                password=password,
-                database=database,
-                port=port
-            ),
-            echo=echo
+            url=f'mssql+pymssql://{user}:{password}@{server}:{port}/{database}?charset=utf8',
+            echo=echo,
         )
         # if not db_adapter.engine.connect():
         #     raise ConnectionError("Could not connect to the database")
