ff1031d367ada164280cb081c0e419a5e8ecc739
Emir <emirsoyalan79@gmail.com>
2024-07-02 16:47:46 +0300

    improve pagerank calculation

--- src/modules/pagerank.py
+++ src/modules/pagerank.py
@@ -6,6 +6,7 @@ from src.database.adapter import load_db_adapter
 from src.models import Config, Document, PageScore
 from src.modules.crawler import Crawler
 from src.modules.document_score_calculator import DocumentScoreCalculator
+from src.modules.normalizer import Normalizer
 from src.services import IPService, PageService
 from src.services.DocumentIndexService import DocumentIndexService
 from src.utils import tag_weights
@@ -26,6 +27,22 @@ page_service = PageService(adapter)
 
 
 class PageRank:
+    _default_weights = {
+        'idf': 0.8,
+        'proximity': 0.5,
+        'weights': 0.3,
+        'authority': 0.1
+    }
+    
+    def __init__(self, weights:dict={}, norm_method=None):
+        self.weights = weights or self._default_weights
+        self.normalization_method = norm_method or Normalizer.z_score
+    
+    def normalize(self, scores):
+        if len(scores) == 0:
+            return scores
+        return self.normalization_method(scores)
+
     def _to_page_score(self, tuples: list[tuple[Document, float]]) -> list[PageScore]:
         return [PageScore(document=doc, idf_score=score) for doc, score in tuples]
     
@@ -40,46 +57,51 @@ class PageRank:
         return self._to_page_score(idf_scores)
 
     def _update_idf_scores_by_domain_authority(self, page_scores: list[PageScore]) -> list[PageScore]:
-        final_scores = []
-
-        # update the idf scores by domain authority
+        idf_scores = [ps.idf_score for ps in page_scores]
+        idf_scores = self.normalize(idf_scores)
+        
+        domain_scores = []
         for page_score in page_scores:
             document = page_score.document
-            page_score = page_score.idf_score
-            
             ip_obj = ip_service.get_ip_by_domain(_get_base_url(document.url))
-            domain_score = 0
-            if ip_obj:
-                domain_score = ip_obj.score
-                
-            if not page_score and not domain_score:
-                score = 0
-            elif not page_score and domain_score:
-                score = domain_score
-            elif page_score and not domain_score:
-                score = page_score
-            elif page_score and domain_score:
-                score = page_score * domain_score
-                
-            final_scores.append(PageScore(document=document, idf_score=score))
+            domain_score = ip_obj.score if ip_obj else 0
+            domain_scores.append(domain_score)
+        domain_scores = self.normalize(domain_scores)
         
-        return final_scores
+        for idx, page_score in enumerate(page_scores):
+            page_score.idf_score = (
+                self.weights['idf'] * idf_scores[idx] +
+                self.weights['authority'] * domain_scores[idx]
+            )
+        
+        return page_scores
     
     def _update_idf_scores_by_tag_weights(self, page_scores: list[PageScore]) -> list[PageScore]:
+        tag_weight_scores = []
         for page_score in page_scores:
-            tag_weight_total = 0
-            for wf in page_score.document.word_frequencies:
-                tag_weight_total += tag_weights.get(wf.tag, 1.0)
-            page_score.idf_score *= tag_weight_total / len(page_score.document.word_frequencies)
+            tag_weight_total = sum(tag_weights.get(wf.tag, 1.0) for wf in page_score.document.word_frequencies)
+            tag_weight_scores.append(tag_weight_total / len(page_score.document.word_frequencies))
+        
+        tag_weight_scores = self.normalize(tag_weight_scores)
+        
+        for idx, page_score in enumerate(page_scores):
+            page_score.idf_score += self.weights['weights'] * tag_weight_scores[idx]
+        
         return page_scores
     
     def _update_idf_scores_by_word_proximity(self, page_scores: list[PageScore]) -> list[PageScore]:
+        proximity_scores = []
         for page_score in page_scores:
             word_locations = defaultdict(list)
             for wf in page_score.document.word_frequencies:
                 word_locations[wf.word].append(wf.location_index)
-            proximity_score = self._calculate_proximity_score(word_locations)
-            page_score.idf_score *= proximity_score
+            proximity_scores.append(self._calculate_proximity_score(word_locations))
+        
+        proximity_scores = self.normalize(proximity_scores)
+        
+        for idx, page_score in enumerate(page_scores):
+            page_score.idf_score += self.weights['proximity'] * proximity_scores[idx]
+        
         return page_scores
     
     def _calculate_proximity_score(self, word_locations):
@@ -91,13 +113,13 @@ class PageRank:
                 distances = [abs(loc1 - loc2) for loc1 in word_locations[words[i]] for loc2 in word_locations[words[j]]]
                 if distances:
                     min_distance = min(min_distance, min(distances))
-                    
-        if min_distance == float('inf'):
+        
+        if (min_distance == float('inf')):
             return 1.0  # No valid distances found
 
         return 1 / (1 + min_distance)  # The smaller the distance, the higher the score
     
-    def _attach_document_metadata(self, page_scores:list[PageScore]):
+    def _attach_document_metadata(self, page_scores: list[PageScore]):
         for page_score in page_scores:
             document = page_service.get_page(page_score.document.url)
             if not document:
